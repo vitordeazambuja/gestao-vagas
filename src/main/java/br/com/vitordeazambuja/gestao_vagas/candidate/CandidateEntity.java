d81278f397ff1cbd05eeb895cbe072a5c7f1573b
Vitor <vitordeazambuja@gmail.com>
2026-01-19 21:08:33 -0300

    chore: criada a tabela do candidato

--- src/main/java/br/com/vitordeazambuja/gestao_vagas/candidate/CandidateEntity.java
+++ src/main/java/br/com/vitordeazambuja/gestao_vagas/candidate/CandidateEntity.java
@@ -1,17 +1,26 @@
 package br.com.vitordeazambuja.gestao_vagas.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
@@ -25,4 +34,7 @@ public class CandidateEntity {
 
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
