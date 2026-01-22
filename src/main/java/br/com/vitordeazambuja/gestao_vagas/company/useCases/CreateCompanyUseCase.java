c9fc4d826f3673087115ad5f5c8cc2df520eba58
Vitor <vitordeazambuja@edu.uniube.br>
2026-01-22 15:46:59 -0300

    feat: implementada criptografia de senha

--- src/main/java/br/com/vitordeazambuja/gestao_vagas/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/vitordeazambuja/gestao_vagas/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import br.com.vitordeazambuja.gestao_vagas.company.entities.CompanyEntity;
 import br.com.vitordeazambuja.gestao_vagas.company.repositories.CompanyRepository;
 import br.com.vitordeazambuja.gestao_vagas.exceptions.UserFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,9 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity) {
 
         this.companyRepository
@@ -19,6 +23,9 @@ public class CreateCompanyUseCase {
                 .ifPresent(user -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
        return this.companyRepository.save(companyEntity);
     }
