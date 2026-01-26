0f4a2132986e8e19cd3e03f27663c198b7fd37e3
Vitor <vitordeazambuja@edu.uniube.br>
2026-01-26 10:01:24 -0300

    feat: criado token de candidato

---
+++ src/main/java/br/com/vitordeazambuja/gestao_vagas/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,5 @@
+package br.com.vitordeazambuja.gestao_vagas.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+
+}
---
+++ src/main/java/br/com/vitordeazambuja/gestao_vagas/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.vitordeazambuja.gestao_vagas.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthCandidateResponseDTO {
+
+    private String access_token;
+}
